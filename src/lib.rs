e7830606af6ba8012032d0b5eb4cb325d504d246
dbregeon <denis.bregeon@digitalbrikes.com>
2020-08-25 07:52:42 +0900

    Initial prototype

---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+extern crate regex;
+extern crate spotr_sensing;
+
+use std::fs;
+use std::result::Result;
+
+use spotr_sensing::{Sensor, Process};
+
+
+use regex::Regex;
+
+#[no_mangle]
+pub fn initialize() -> *mut dyn Sensor<Item = Process> {
+    Box::into_raw(Box::new(ProcessSensor))
+}
+
+
+struct ProcessSensor;
+
+impl ProcessSensor {
+    fn read_proc<I>(paths: I) -> Vec<Process> where I: Iterator<Item = Result<fs::DirEntry, std::io::Error>> {
+        let input_re = Regex::new(r#"^(\d+)$"#).unwrap();
+        let mut processes = vec!();
+        for result in paths {
+            match result {
+                Ok(entry) => {
+                    let name = entry.file_name().into_string().unwrap();
+                    for capture in input_re.captures_iter(name.as_str()) {
+                        let pid = capture[1].parse::<u32>().unwrap();
+                        processes.push(Process { pid });
+                    }
+                },
+                Err(_) => {}
+            };
+        }
+        processes
+    }
+}
+
+impl Sensor for ProcessSensor {
+    type Item = Process;
+
+    fn sample(&self) -> Result<Vec<Process>, std::io::Error> {
+        let proc_read_result = fs::read_dir("/proc");
+
+        match proc_read_result {
+            Ok(paths) => Ok(ProcessSensor::read_proc(paths)),
+            Err(e) => Err(e)
+        }
+    }
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn read_proc_creates_a_process_sensor_with_empty_processes() {
+        let tmp_dir = std::env::temp_dir().join("spotr1");
+        std::fs::create_dir_all(&tmp_dir).unwrap();
+
+        let processes = super::ProcessSensor::read_proc(std::fs::read_dir(&tmp_dir).unwrap());
+
+        std::fs::remove_dir_all(&tmp_dir).unwrap();
+
+        assert_eq!(0, processes.len());
+    }
+
+    #[test]
+    fn read_proc_creates_a_process_sensor_with_processes_for_each_numeric_directory() {
+        let tmp_dir = std::env::temp_dir().join("spotr2");
+        std::fs::create_dir_all(tmp_dir.join("12345")).unwrap();
+        std::fs::create_dir_all(tmp_dir.join("54321")).unwrap();
+
+        let processes = super::ProcessSensor::read_proc(std::fs::read_dir(&tmp_dir).unwrap());
+
+        std::fs::remove_dir_all(&tmp_dir).unwrap();
+
+        assert_eq!(2, processes.len());
+        let mut pids: Vec<u32> = processes.into_iter().map(|p| p.pid).collect();
+        pids.sort();
+        assert_eq!(12345, pids[0]);
+        assert_eq!(54321, pids[1]);
+    }
+
+    #[test]
+    fn read_proc_creates_a_process_sensor_ignores_non_numeric_directories() {
+        let tmp_dir = std::env::temp_dir().join("spotr3");
+        std::fs::create_dir_all(tmp_dir.join("p1234")).unwrap();
+        std::fs::create_dir_all(tmp_dir.join("1234p")).unwrap();
+        std::fs::create_dir_all(tmp_dir.join("test")).unwrap();
+
+        let processes = super::ProcessSensor::read_proc(std::fs::read_dir(&tmp_dir).unwrap());
+
+        std::fs::remove_dir_all(&tmp_dir).unwrap();
+
+        assert_eq!(0, processes.len());
+    }
+}
